4e88a9292d911ce79cdb832905f6352884f79a4c
afropapp13 <afropapp@yahoo.gr>
2024-11-14 09:11:38 -0600

    Nov 14 2024: median calculation

--- myFunctions.cpp
+++ myFunctions.cpp
@@ -9,11 +9,25 @@
 #include <cmath>
 #include <TVectorD.h>
 #include <TDecompSVD.h>
+#include <vector>
 
 #include "Constants.h"
 
 using namespace std;
 using namespace Constants;
+
+//----------------------------------------//
+
+double Median(const TH1D * h1) { 
+
+	int n = h1->GetXaxis()->GetNbins();  
+	std::vector<double>  x(n);
+	h1->GetXaxis()->GetCenter( &x[0] );
+	const double * y = h1->GetArray(); 
+	// exclude underflow/overflows from bin content array y
+   	return TMath::Median(n, &x[0], &y[1]); 
+   
+}
 
 //----------------------------------------//
 
